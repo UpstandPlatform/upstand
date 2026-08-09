2e4dad777f1bf8373f2bc1e2491a1fb7793ea61b
mhbdev <whw1483@gmail.com>
2026-08-09 18:38:50 +0330

    test: isolate production crypto and database suites

--- packages/usecases/src/notification/update-notification-channel.usecase.test.ts
+++ packages/usecases/src/notification/update-notification-channel.usecase.test.ts
@@ -8,11 +8,11 @@ import { encryptSecret } from "@upstand/platform/crypto/secret-box";
 import { UpdateNotificationChannelUseCase } from "./update-notification-channel.usecase";
 
 const TEST_KEY = Buffer.alloc(32, 7).toString("base64");
+process.env.ENCRYPTION_KEY_V1 ??= TEST_KEY;
 
 function createChannel(
   configuration: NotificationConfiguration,
 ): NotificationChannel {
-  process.env.ENCRYPTION_KEY_V1 = TEST_KEY;
   return {
     id: "channel-1",
     organizationId: "org-1",
