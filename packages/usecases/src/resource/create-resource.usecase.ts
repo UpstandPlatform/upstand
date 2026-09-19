15aeb1736df8a71ae4b34763a10bc1e38ec95533
mhbdev <whw1483@gmail.com>
2026-09-20 01:50:31 +0330

    chore(release): resolve canary promotion merge

--- packages/usecases/src/resource/create-resource.usecase.ts
+++ packages/usecases/src/resource/create-resource.usecase.ts
@@ -18,7 +18,6 @@ import {
 import { encryptSecret } from "@upstand/platform/crypto/secret-box";
 import { log } from "evlog";
 import { z } from "zod";
-import { requiresRemoteServerPlacement } from "../platform/platform.types";
 import {
   requiresRemoteDeploymentServer,
   requiresRemoteServerPlacement,
